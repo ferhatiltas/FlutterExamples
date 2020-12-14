69e74b3a6400613381f0e29e043260625e101cd3
Ferhat İltaş <iltasferhat@gmail.com>
2020-12-14 23:48:55 +0300

    interface changed

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text("Flutter Örnekleri"),
       ),
-        body: ResimveButonTurleri()
+        body: RowColumnOdev()
     ),
     );
   }
