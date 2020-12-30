8c179f1bb37d8a6cb470aabcf91a470c18152849
Ferhat İltaş <iltasferhat@gmail.com>
2020-12-30 23:13:37 +0300

    Lists Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ornek_image_text/ui/etkin_liste_ornek.dart';
 
 
-void main() => runApp(MyApp());
-
-
-
+void main() => runApp(MaterialApp(
+      title: "Flutter Examples",
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List Examples"),
+        ),
+        body: EtkinListeOrnek(),
+      ),
+    ));
