0fef1e15e917ace0971d28c5ade84360181d0d56
Ferhat İltaş <iltasferhatt@gmail.com>
2021-02-02 01:42:01 +0300

    Add files via upload

---
+++ lib/ui/kilit_sayfasi.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class KilitSayfasi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlue,
+      child: Center(child: Text("Kilit Sayfası"),),
+    );
+  }
+}
